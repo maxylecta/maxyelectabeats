9a62340b07d086c9a4237d4cf2ccf12353784559
Maxy Electa <max51450@gmail.com>
2025-06-03 17:48:40 -0400

    Update from Bolt - ajout de beats

--- src/components/CustomBeatRequestModal.tsx
+++ src/components/CustomBeatRequestModal.tsx
@@ -38,7 +38,7 @@ const CustomBeatRequestModal: React.FC<CustomBeatRequestModalProps> = ({ isOpen,
     setIsSubmitting(true);
 
     try {
-      const response = await fetch('https://maxyelectazone.app.n8n.cloud/webhook-test/6d5c6048-7f93-4616-93dd-0e6b93f5ee49', {
+      const response = await fetch('https://maxyelectazone.app.n8n.cloud/webhook/6d5c6048-7f93-4616-93dd-0e6b93f5ee49', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
